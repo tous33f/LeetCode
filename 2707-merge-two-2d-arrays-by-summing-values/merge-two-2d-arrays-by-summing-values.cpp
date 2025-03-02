dc56886180eb2c44f656b44bc363344844cbe353
Muhammad Touseef <125975010+tous33f@users.noreply.github.com>
2025-03-02 14:08:27 +0500

    Time: 2 ms (39.65%) | Memory: 14.7 MB (64.62%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -2,58 +2,32 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         int n1=nums1.size(),n2=nums2.size();
+        int i=0,j=0,key,value;
         vector<vector<int>>ans;
-        unordered_map<int,int>idxs;
-        int i=0,j=0,key,value,cur=0;
         while(i<n1 && j<n2){
             if(nums1[i][0]<nums2[j][0]){
                 key=nums1[i][0];
-                value=nums1[i][1];
-                i++;
+                value=nums1[i++][1];
             }
             else if(nums1[i][0]>nums2[j][0]){
                 key=nums2[j][0];
-                value=nums2[j][1];
-                j++;
-            }
-            else {
-                key=nums1[i][0];
-                value=nums1[i][1]+nums2[j][1];
-                i++;
-                j++;
-            }
-            if(idxs.find(key)==idxs.end()){
-                ans.push_back(vector<int>{key,value});
-                idxs[key]=cur++;
+                value=nums2[j++][1];
             }
             else{
-                ans[idxs[key]][1]+=value;
+                key=nums1[i][0];
+                value=nums1[i++][1]+nums2[j++][1];
             }
-            cout<<key<<","<<value<<endl;
+            ans.push_back(vector<int>{key,value});
         }
         while(i<n1){
             key=nums1[i][0];
-            value=nums1[i][1];
-            i++;
-            if(idxs.find(key)==idxs.end()){
-                ans.push_back(vector<int>{key,value});
-                idxs[key]=cur++;
-            }
-            else{
-                ans[idxs[key]][1]+=value;
-            }
+            value=nums1[i++][1];
+            ans.push_back(vector<int>{key,value});
         }
         while(j<n2){
             key=nums2[j][0];
-            value=nums2[j][1];
-            j++;
-            if(idxs.find(key)==idxs.end()){
-                ans.push_back(vector<int>{key,value});
-                idxs[key]=cur++;
-            }
-            else{
-               ans[idxs[key]][1]+=value;
-            }
+            value=nums2[j++][1];
+            ans.push_back(vector<int>{key,value});
         }
         return ans;
     }
