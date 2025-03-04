2003e6aa4d7647fde449416cff25a72660a544fd
Muhammad Touseef <125975010+tous33f@users.noreply.github.com>
2025-03-04 14:31:32 +0500

    Time: 3 ms (12.35%) | Memory: 12.3 MB (81.45%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,8 +5,33 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        for i in range( len(haystack) ):
-            print(haystack[i:i+len(needle)],needle)
-            if haystack[i]==needle[0] and haystack[i:i+len(needle)]==needle:
-                return i
+        lps=[0]*len(needle)
+        i=1
+        l=0
+        while i<len(needle):
+            if needle[l]==needle[i]:
+                l+=1
+                lps[i]=l
+                i+=1
+            else:
+                if l==0:
+                    lps[i]=l
+                    i+=1
+                else:
+                    l=lps[l-1]
+        i=0
+        j=0
+        print(lps)
+        while i<len(haystack):
+            if haystack[i]==needle[j]:
+                i+=1
+                j+=1
+            else:
+                if j==0:
+                    i+=1
+                else:
+                    j=lps[j-1]
+            if j==len(needle):
+                return i-j
         return -1
+        
