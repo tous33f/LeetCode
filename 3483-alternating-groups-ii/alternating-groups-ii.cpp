5fa4c31d9116b8bc01298fbf279ecc67bc5d5c27
Muhammad Touseef <125975010+tous33f@users.noreply.github.com>
2025-03-09 14:25:09 +0500

    Time: 79 ms (27.20%) | Memory: 108.1 MB (38.70%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        /*-------------------- OPTIMIZED SOL --------------------*/
+        for(int i=0;i<k-1;i++) colors.push_back(colors[i]);
+        int n=colors.size(),cur=1;
+        int cnt=0,win=1;
+        while(cur<n){
+            if(colors[cur]!=colors[cur-1]){
+                win++;
+            }
+            else{
+                int t=win-k+1;
+                cnt+=(t>0?t:0);
+                win=1;
+            }
+            cur++;
+        }
+        int t=win-k+1;
+        cnt+=(t>0?t:0);
+        return cnt;
+
+        /*-------------------- UNOPTIMIZED SOL --------------------*/
+        // int cnt=0;
+        // int n=colors.size();
+        // int cur=0;
+        // cout<<n<<endl;
+        // do{
+        //     bool flag=1;
+        //     for(int i=(cur+1)%n,j=0;j<k-1;i=(i+1)%n,j++){
+        //         // cout<<((i-1)+n)%n<<","<<i<<" ";
+        //         if(colors[i]==colors[((i-1)+n)%n]){
+        //             if(((i-1)+n)%n>=cur) cur=((i-1)+n)%n;
+        //             else cur=n-1;
+        //             flag=0;
+        //             break;
+        //         }
+        //     }
+        //     // cout<<endl;
+        //     if(flag) cnt++;
+        //     cur=(cur+1)%n;
+        //     cout<<cur<<endl;
+        // }while(cur!=0);
+        // return cnt;
+    }
+};
